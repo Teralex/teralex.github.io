c1967e910580126fba4b4704969f0a8aa6cf15c4
User <User>
2017-05-22 12:23:15 +0300

    Test

--- front/js/app.js
+++ front/js/app.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $(".banner > img").show();
     $(".next").bind({
         click: function () {
             //$(".inv-nav-circle:contains(" + inviteObject.step + ")").addClass('active');
@@ -8,11 +9,13 @@ $(document).ready(function () {
     var step = 1;
     function setStep(el) {
         $("#step" + step).removeClass('active').addClass('closed');
-        $("#step" + step).find('img').attr("src","images/arr_down.png");;
-        
+        $("#step" + step).find('img').attr("src", "images/arr_down.png");
+        ;
+
         step = Number(el.id.replace(/\D+/g, ""))
 
         $("#step" + step).addClass('active');
-        $("#step" + step).find('img').attr("src","images/active_arr.png");;
+        $("#step" + step).find('img').attr("src", "images/active_arr.png");
+        ;
     }
 });
